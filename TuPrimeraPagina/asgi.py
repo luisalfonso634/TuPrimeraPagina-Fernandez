3f77f014d59cb87ff1515b02f87fdcb9128b930a
Luis.Fernandez2 <luisalfonso634@gmail.com>
2025-05-26 13:42:21 -0300

    Segundo Envio Tarea 3

--- TuPrimeraPagina/asgi.py
+++ TuPrimeraPagina/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for TuPrimeraPagina project.
+ASGI config for TuPrimeraPagina2 project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'TuPrimeraPagina.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'TuPrimeraPagina2.settings')
 
 application = get_asgi_application()
--- TuPrimeraPagina/wsgi.py
+++ TuPrimeraPagina/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for TuPrimeraPagina project.
+WSGI config for TuPrimeraPagina2 project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'TuPrimeraPagina.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'TuPrimeraPagina2.settings')
 
 application = get_wsgi_application()
